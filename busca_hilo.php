0962f32a68159f39da8dbf5e61e104ca8fc681e9
Julio Cesar Barradas Rodriguez <julio_br_02@hotmail.com>
2019-04-03 13:36:00 -0600

    Se Añadio el Scritp que permite ingresar la calve del Hilo y como resultado pone el Nombre del Hilo

--- busca_hilo.php
+++ busca_hilo.php
@@ -8,7 +8,7 @@
   $mysqli = new mysqli($host,$username,$password,$databasename);
   $mysqli->set_charset("utf8");
 
-  $consulta = "SELECT UPPER(A.descripcion) as descripcion FROM existencia A ORDER BY A.descripcion";
+  $consulta = "SELECT UPPER(A.descripcion) as descripcion FROM existencia A WHERE A.hilo = ". $_POST['idhilo'] ."LIMIT 1";
 
   if ($resultado = $mysqli->query($consulta)) {
     while ($obj = $resultado->fetch_object()) {
@@ -20,6 +20,6 @@
   }
 
   $mysqli->close();
-
+  //echo ($data);
   echo json_encode($data,JSON_UNESCAPED_UNICODE);
 ?>
--- index.php
+++ index.php
@@ -8,12 +8,41 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="/resources/demos/style.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script >
-      $( function() {
 
+    <script >
+      // Busca el Nombre del Hilo usando su Clave
+      $( function() {
+        $( "#clave_hilo" ).keypress(function( event ) {
+          if ( event.which == 13 ) {
+             event.preventDefault();
+
+             var idhilo_var = parseFloat($('input[id=clave_hilo]').val()).toFixed(2);
+             $.ajax({
+                 url: "busca_hilo.php",
+                 method: "POST",
+                 data: {
+                   idhilo : idhilo_var
+                 },
+                 success: function(r){
+                     $('input[id=hilos]').val(r) ;
+                 },
+                 dataType: "json"
+             });
+            //$('input[id=supervisor]').val('julio cesar');
+          }
+        });
+
+        $( "#idsupervisor" ).click(function() {
+          $( "#idsupervisor" ).keypress();
+        });
+      } );
+    </script>
+
+    <script >
+      // Escrip de Autocompletar
+      $( function() {
         var lista_hilos = [];
         // retrieve JSon from external url and load the data inside an array :
         $.getJSON( "autocompletar.php", function( data ) {
@@ -54,7 +83,7 @@
             <div class="col-xs-1">
               <div class="form-group">
                   <label>ID</label>
-                  <input type="text" id="idsupervisor" class="form-control" />
+                  <input type="text" id="idsupervisor" class="form-control" disabled/>
               </div>
             </div>
             <div class="col-xs-6">
@@ -77,8 +106,8 @@
 
           <div class="row">
             <div class="col-xs-1">
-              <label>ID</label>
-              <input type="text" id="clave_hilo" value="0" class="form-control" disabled/>
+              <label>Clave</label>
+              <input type="text" id="clave_hilo" value="0" class="form-control" />
             </div>
             <div class="col-xs-7">
               <label>Hilo</label>
