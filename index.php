59f804721ab4bb8d21d82cff2a6c1ecb0d0a3018
jucebaro <julio_br_02@hotmail.com>
2019-04-13 08:47:41 -0500

    Cambie a local los archivos de css y js

--- index.php
+++ index.php
@@ -14,12 +14,11 @@
   <meta charset="utf-8" />
   <link rel="stylesheet" type="text/css" href="css/bootstrap3-3-7.min.css">
   <link rel="stylesheet" type="text/css" href="css/bootstrap3-3-7-theme.min.css">
-
+  <link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
   <link rel="stylesheet" type="text/css" href="css/padding.css">
 
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script src="js/jquery-ui1-12-1.js"></script>
+  <script src="js/jquery-1-12-4.js"></script>
 </head>
 
 <body>
