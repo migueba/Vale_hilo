6e20ea108b2ce109fd9b050673c7f865c0bfe598
jucebaro <julio_br_02@hotmail.com>
2019-04-23 23:36:33 -0500

    puse un nvar asi, como corregi la redirecion en la parte de vale

--- index.php
+++ index.php
@@ -27,6 +27,25 @@
 
 <body>
     <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light bg-primary">
+        <img style="height: 50px; width: 110px; display: block; margin-right:30px;" src="images/FABRICA MARÍA SIN FONDO_negro_corta.png" />
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php"><b>Crear Vale</b> <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="vales.php"><b>Vales</b></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
         <h2 class="page-header">Vale de Hilo</h2>
         <form method="POST" id="formulario" action="modelo/guardar_vale.php">
           <div class="row">
--- vales.php
+++ vales.php
@@ -21,6 +21,25 @@
 </head>
 <body>
     <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light bg-primary">
+        <img style="height: 50px; width: 110px; display: block; margin-right:30px;" src="images/FABRICA MARÍA SIN FONDO_negro_corta.png" />
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php"><b>Crear Vale </b><span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="vales.php"><b>Vales</b></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
         <h2 class="page-header">Vales de Hilo Registrados</h2>
           <div class="row">
             <div class="col-lg-12" >
@@ -73,7 +92,7 @@
 
         $("#contenido").on('click', '.clickojo', function(data) {
           var id_vale = $(this).attr('id-vale') ;
-          window.open('http://192.168.1.13/vale_hilo/modelo/ver_vale.php?id_vale='+id_vale, '_blank');
+          window.open('modelo/ver_vale.php?id_vale='+id_vale, '_blank');
         });
 
       });
