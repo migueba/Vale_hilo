9596cdb6f1d21e9597deeef63885654a952caab8
Julio Cesar Barradas Rodriguez <julio_br_02@hotmail.com>
2019-05-07 12:29:39 -0500

    Pruebas con la graficas de Chart.js

--- js/canvas/grafica_index.js
+++ js/canvas/grafica_index.js
@@ -1,23 +1,25 @@
-var $hilos = new Array();
+var hilos = new Array();
+var peso = new Array();
 $.getJSON( "modelo/inventario.php?function=inventario_hilo",
   function( data ) {
     $.each( data, function( key, val ) {
-      console.log(val.descripcion);
-      console.log(val.pesoneto_cal);
+      hilos.push(val.descripcion.trim());
+      peso.push(parseFloat(val.pesoneto_cal));
     });
+
+    var ctx = document.getElementById('myAreaChart').getContext('2d');
+    var config = {
+       type: 'bar',
+       data: {
+          labels: hilos ,
+          datasets: [{
+             label: 'Inventario',
+             data: peso ,
+             backgroundColor: 'rgba(0, 119, 204, 0.3)',
+             borderWidth: 1
+          }]
+       }
+    };
+    var chart = new Chart(ctx, config);
   }
 );
-
-var ctx = document.getElementById('myAreaChart').getContext('2d');
-var config = {
-   type: 'line',
-   data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-         label: 'Graph Line',
-         data: [12, 19, 3, 5, 2, 3],
-         backgroundColor: 'rgba(0, 119, 204, 0.3)'
-      }]
-   }
-};
-var chart = new Chart(ctx, config);
