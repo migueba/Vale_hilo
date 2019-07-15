3fd7fc1beec51d7a9012cd0c2925c8b43b8bb9eb
Julio Cesar Barradas Rodriguez <julio_br_02@hotmail.com>
2019-07-15 18:26:19 -0500

    agregada la funcion para consultar, las construcciones de las telas

---
+++ js/construccion/javascrtip.js
@@ -0,0 +1,108 @@
+$( function() {
+  /////// Cuando preciona el Boton de Guardar al Formulario
+  var form = $("#formulario");
+  form.submit(function(){
+      event.preventDefault();
+      $( "#waiting" ).show( "slow" ) ;
+      $.ajax({
+          url: "modelo/inf_construccion.php",
+          method: "GET",
+          data: form.serialize(),
+          dataType: "json",
+          success: function(r){
+            if(r.length > 0) {
+              $.each(r, function(key, value){
+                $("#inf-vale").html('') ;
+                $("#inf-vale").append("<div class=\"row\">"+
+                    "<div class=\"col-xl-2 col-lg-2 col-md-2 col-sm-3\">"+
+                      "<div class=\"form-group\">"+
+                        "<label>CLAVE TG</label>"+
+                        "<input type=\"text\" id=\"clave_tg\" class=\"form-control\" name=\"clave_tg\" value=\""+value.clave+"\" readonly />"+
+                      "</div>"+
+                    "</div>"+
+                    "<div class=\"col-xl-6 col-lg-6 col-md-10 col-sm-9\">"+
+                      "<div class=\"form-group\">"+
+                        "<label>TELA</label>"+
+                        "<input type=\"text\" id=\"tela\" class=\"form-control\" name=\"tela\" value=\""+value.tela+"\" readonly />"+
+                      "</div>"+
+                    "</div>"+
+                  "</div>"+
+                  "<div class=\"row\">"+
+                    "<div class=\"col-xl-3 col-lg-3 col-md-3 col-sm-3\">"+
+                      "<div class=\"form-group\">"+
+                        "<label>Hilo PIE</label>"+
+                        "<input type=\"text\" id=\"h_pie\" class=\"form-control\" name=\"h_pie\" value=\""+value.pie+"\" readonly />"+
+                      "</div>"+
+                    "</div>"+
+                    "<div class=\"col-xl-3 col-lg-3 col-md-3 col-sm-3\">"+
+                      "<div class=\"form-group\">"+
+                        "<label>Hilo TRAMA</label>"+
+                        "<input type=\"text\" id=\"h_trama\" class=\"form-control\" name=\"h_trama\" value=\""+value.trama+"\" readonly />"+
+                      "</div>"+
+                    "</div>"+
+                    "<div class=\"col-xl-3 col-lg-3 col-md-3 col-sm-3\">"+
+                      "<div class=\"form-group\">"+
+                        "<label>Ancho PEINE</label>"+
+                        "<input type=\"text\" id=\"h_trama\" class=\"form-control\" name=\"h_trama\" value=\""+value.anchopeine+"\" readonly  />"+
+                      "</div>"+
+                    "</div>"+
+                    "<div class=\"col-xl-3 col-lg-3 col-md-3 col-sm-3\">"+
+                      "<div class=\"form-group\">"+
+                        "<label>Hilos PEINE</label>"+
+                        "<input type=\"text\" id=\"h_trama\" class=\"form-control\" name=\"h_trama\" value=\""+value.hilopeine+"\" readonly />"+
+                      "</div>"+
+                    "</div>"+
+                  "</div>"+
+                  "<div class=\"row\">"+
+                    "<div class=\"col-xl-3 col-lg-3 col-md-3 col-sm-3\">"+
+                      "<div class=\"form-group\">"+
+                        "<label>Luchas x Pulg.</label>"+
+                        "<input type=\"text\" id=\"h_pie\" class=\"form-control\" name=\"h_pie\" value=\""+value.luchaxpulgada+"\" readonly />"+
+                      "</div>"+
+                    "</div>"+
+                    "<div class=\"col-xl-3 col-lg-3 col-md-3 col-sm-3\">"+
+                      "<div class=\"form-group\">"+
+                        "<label>Ancho en CM.</label>"+
+                        "<input type=\"text\" id=\"h_trama\" class=\"form-control\" name=\"h_trama\" value=\""+value.anchocm+"\" readonly />"+
+                      "</div>"+
+                    "</div>"+
+                    "<div class=\"col-xl-3 col-lg-3 col-md-3 col-sm-3\">"+
+                      "<div class=\"form-group\">"+
+                        "<label>Pie-Trama-Apre</label>"+
+                        "<input type=\"text\" id=\"h_trama\" class=\"form-control\" name=\"h_trama\" value=\""+value.pesopta+"\" readonly />"+
+                      "</div>"+
+                    "</div>"+
+                    "<div class=\"col-xl-3 col-lg-3 col-md-3 col-sm-3\">"+
+                      "<div class=\"form-group\">"+
+                        "<label>Peso g/m2</label>"+
+                        "<input type=\"text\" id=\"h_trama\" class=\"form-control\" name=\"h_trama\" value=\""+value.pesogm2+"\" readonly />"+
+                      "</div>"+
+                    "</div>"+
+                    "<div class=\"row\">"+
+                      "<div class=\"col-xl-3 col-lg-3 col-md-3 col-sm-3\">"+
+                        "<label>Hilos X Pulg.</label>"+
+                        "<input type=\"text\" id=\"h_trama\" class=\"form-control\" name=\"h_trama\" value=\""+value.hxpulg+"\" readonly />"+
+                      "</div>"+
+                      "<div class=\"col-xl-3 col-lg-3 col-md-3 col-sm-3\">"+
+                        "<label>Hilos X Pulg. 2</label>"+
+                        "<input type=\"text\" id=\"h_trama\" class=\"form-control\" name=\"h_trama\" value=\""+value.hxpulg2+"\" readonly />"+
+                      "</div>"+
+                    "</div>"+
+                  "</div>");
+              });
+              $( "#waiting" ).hide( "slow" );
+            }else{
+              $("#waiting" ).hide( "slow" );
+              $("#inf-vale").html('') ;
+              $("#inf-vale").append("<span class=\"badge badge-danger\">La tela Ingresada No Existe</span>");
+            }
+          },
+          error: function(r){
+            $("#waiting" ).hide( "slow" );
+            $("#inf-vale").html('') ;
+            alert("No se puede Mostrar la informacion de la Tela");
+          }
+      });
+      return false;
+  });
+});
--- modelo/inf_construccion.php
+++ modelo/inf_construccion.php
@@ -21,42 +21,30 @@
     // Get the highest row and column numbers referenced in the worksheet
     $highestRow = $sheet->getHighestRow(); // e.g. 10
 
+    $TelasArray = array() ;
+
     for ($row = 6; $row <= $highestRow; ++$row) { // Comienza a leer desde el 6
-            $clave = $sheet->getCellByColumnAndRow(2, $row)->getValue();
-            $tela =  strtoupper($sheet->getCellByColumnAndRow(3, $row)->getValue());
-            $pie =  $sheet->getCellByColumnAndRow(4, $row)->getValue();
-            $trama =  $sheet->getCellByColumnAndRow(6, $row)->getValue();
-            $Anchopeine = $sheet->getCellByColumnAndRow(8, $row)->getValue();
-            $hilopeine = $sheet->getCellByColumnAndRow(10, $row)->getValue();
-            $luchapulgada = $sheet->getCellByColumnAndRow(14, $row)->getValue();
-            $anchocm = $sheet->getCellByColumnAndRow(15, $row)->getValue();
-            $pesoPTA =  $sheet->getCellByColumnAndRow(20, $row)->getCalculatedValue();
-            $pesogm2 =  $sheet->getCellByColumnAndRow(24, $row)->getCalculatedValue();
+      if ( $sheet->getCellByColumnAndRow(2, $row)->getValue() === strtoupper($_GET['clave']) ){
+        $data['clave'] = $sheet->getCellByColumnAndRow(2, $row)->getValue() ;
+        $data['tela'] = strtoupper($sheet->getCellByColumnAndRow(3, $row)->getValue()) ;
+        $data['pie'] = $sheet->getCellByColumnAndRow(4, $row)->getValue() ;
+        $data['trama'] = $sheet->getCellByColumnAndRow(6, $row)->getValue() ;
+        $data['anchopeine'] = $sheet->getCellByColumnAndRow(8, $row)->getValue() ;
+        $data['hilopeine'] = $sheet->getCellByColumnAndRow(10, $row)->getValue() ;
+        $data['luchaxpulgada'] = $sheet->getCellByColumnAndRow(14, $row)->getValue() ;
+        $data['anchocm'] = $sheet->getCellByColumnAndRow(15, $row)->getValue() ;
+        $data['pesopta'] = $sheet->getCellByColumnAndRow(20, $row)->getCalculatedValue() ;
+        $data['pesogm2'] = round($sheet->getCellByColumnAndRow(24, $row)->getCalculatedValue()) ;
 
-            if(!is_null($clave) && !is_null($tela) && !is_null($pie) && !is_null($trama) ){
-              if (gettype($pie) != 'string' ){
-                $pie = strval($pie) ;
-              }
-              if (gettype($trama) != 'string' ){
-                $trama = strval($trama) ;
-              }
+        $data['hxpulg'] = round($sheet->getCellByColumnAndRow(12, $row)->getCalculatedValue()) ;
+        $data['hxpulg2'] = round($sheet->getCellByColumnAndRow(13, $row)->getCalculatedValue()) ;
 
-              $data['clave'] = $clave ;
-              $data['tela'] = $tela ;
-              $data['pie'] = $pie ;
-              $data['trama'] = $trama ;
-              $data['anchopeine'] = $Anchopeine ;
-              $data['hilopeine'] = $hilopeine ;
-              $data['luchaxpulgada'] = $luchapulgada ;
-              $data['anchocm'] = $anchocm ;
-              $data['pesopta'] = $pesoPTA ;
-              $data['pesogm2'] = round($pesogm2) ;
-
-              array_push($TelasArray, $data);
-            }
+        array_push($TelasArray, $data);
+        break;
+      }
     }
-    asort($TelasArray);
-    var_dump($TelasArray);
+    header('Content-Type: application/json');
+    echo json_encode($TelasArray,JSON_UNESCAPED_UNICODE);
   }
 
 ?>
